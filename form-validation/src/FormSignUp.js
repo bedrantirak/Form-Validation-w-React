3a9d033711dc4c7b89df58e6ae89c50c929223f3
bedrantirak <bdrntrk@gmail.com>
2020-10-21 12:15:21 +0300

    Update FormSignUp.js

--- form-validation/src/FormSignUp.js
+++ form-validation/src/FormSignUp.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import useForm from "./useForm";
 import validate from "./validateInfo";
+import "./Form.css";
 
 const FormSignup = () => {
 
-    const {handleChange,values,handleSubmit} = useForm(validate);
+    const {handleChange,values,handleSubmit,errors} = useForm(validate);
 
     return (
         <div className="form-content-right">
@@ -12,7 +13,7 @@ const FormSignup = () => {
                <h1>Get started with us today! Create your
                    account by filling out the information below.
                </h1>
-               <div className="form-imputs">
+               <div className="form-inputs">
                  <label htmlFor="username" className="form-label">
                   Username
                  </label>
@@ -20,14 +21,16 @@ const FormSignup = () => {
                  id="username"
                  type="text" 
                  name="username"
-                 className="form-imput"
+                 className="form-input"
                  placeholder="Enter your username"
                  value={values.username}
                  onChange={handleChange}
-                 />  
+                 /> 
+             {errors.username && <p>{errors.username}</p>}
+
                </div>
 
-               <div className="form-imputs">
+               <div className="form-inputs">
                  <label htmlFor="email" className="form-label">
                 E-mail
                  </label>  
@@ -35,15 +38,17 @@ const FormSignup = () => {
                  id="email"
                  type="email" 
                  name="email"
-                 className="form-imput"
+                 className="form-input"
                  placeholder="Enter your email"
                  value={values.email}
                  onChange={handleChange}
                  />
 
+              {errors.email && <p>{errors.email}</p>}
+
                </div>
                
-               <div className="form-imputs">
+               <div className="form-inputs">
                  <label htmlFor="password" className="form-label">
                 Password
                  </label>  
@@ -51,15 +56,17 @@ const FormSignup = () => {
                  id="password"
                  type="password" 
                  name="password"
-                 className="form-imput"
+                 className="form-input"
                  placeholder="Enter your password"
                  value={values.password}
                  onChange={handleChange}
                  />
 
+              {errors.password && <p>{errors.password}</p>}
+
                </div>
                
-               <div className="form-imputs">
+               <div className="form-inputs">
                  <label htmlFor="password2" className="form-label">
                 Confirm Password
                  </label>  
@@ -67,11 +74,13 @@ const FormSignup = () => {
                  id="password2"
                  type="password" 
                  name="password2"
-                 className="form-imput"
+                 className="form-input"
                  placeholder="Enter your password2"
                  value={values.password2}
                  onChange={handleChange}
                  />
+
+               {errors.password2 && <p>{errors.password2}</p>}
 
                </div>
                <button className="form-input-btn"
