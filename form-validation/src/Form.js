d492502fd98b2b7183665a6c262cc179ef4eb778
bedrantirak <bdrntrk@gmail.com>
2020-10-21 15:38:23 +0300

    Update Form.js

--- form-validation/src/Form.js
+++ form-validation/src/Form.js
@@ -1,19 +1,25 @@
 import React, { useState } from 'react'
 import FormSignup from './FormSignup'
 import FormSuccess from "./FormSuccess"
+import "./Form.css"
 const Form = () => {
-    const [isSubmitted,setIsSubmitted]=useState(false)
+    const [isSubmitted,setIsSubmitted]=useState(false);
 
     function submitForm(){
         setIsSubmitted(true);
     }
     return (
-        <div>
-            {!isSubmitted ? (<FormSignup submitForm={submitForm} />):(<FormSuccess/>) }
-        
+        <>
+        <div className="form-container">
+            <span className="close-btn">x</span>
+        <div className="form-content-left">
+            <img src="img/img-2.svg" alt="spaceship" className="form-img"></img>
         </div>
-        
-    )
-}
+        {!isSubmitted ? (<FormSignup submitForm={submitForm} />):(<FormSuccess/>) }
+        </div>
+       
+        </>
+    );
+};
 
-export default Form
+export default Form;
